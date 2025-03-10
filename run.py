5223a49220b249fe6310e434274e8e4d4c0e46eb
swagatobhaskar <bhaskarswagato@gmail.com>
2025-03-11 00:59:18 +0530

    Edit: minor

--- run.py
+++ run.py
@@ -4,4 +4,5 @@ if __name__ == '__main__':
     app = create_app()
     # Do not use run() in a production setting. It is not intended to
     # meet security and performance requirements for a production server.
-    app.run(debug=True) # debug=app.config['DEBUG']
+    app.run(host="0.0.0.0", debug=True) # debug=app.config['DEBUG']
+    
